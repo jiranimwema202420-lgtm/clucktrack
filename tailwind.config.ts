1b6d4952a0902da23c7f6c6cb433f78afa44b708
Nduati Njoro <jiranimwema202420@gmail.com>
2025-11-29 11:09:35 +0000

    Styling failing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,6 @@ export default {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"PT Sans"', 'sans-serif'],
-        code: ['monospace'],
-      },
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
