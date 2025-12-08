e85c5506fe3801607412c8080c11d9dad5a9bcfb
Nduati Njoro <jiranimwema202420@gmail.com>
2025-12-08 05:40:21 +0000

    experimental: {   allowedDevOrigins: [...] }

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
+  devServer: {
     allowedDevOrigins: ["*.cloudworkstations.dev"],
   },
   images: {
