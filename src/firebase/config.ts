64c6071eb2b9b39b97900779b0a0195f481e9979
Nduati Njoro <jiranimwema202420@gmail.com>
2025-12-10 10:23:46 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/api

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "clucktrack-6i22",
   "appId": "1:95995963953:web:06b521b4f4f3b79417387d",
-  "apiKey": "AIzaSyB1d-3V2y2oY5xV8z7y8x9y0z1y2x3y4z",
+  "apiKey": "AIzaSyC_jGg4gkjfldksalkdfjaoijwelkjsad",
   "authDomain": "clucktrack-6i22.firebaseapp.com",
   "measurementId": "G-5G6H7J8K9L",
   "messagingSenderId": "95995963953",
