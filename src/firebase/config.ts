efe19818926cb20be9bbc3485ca816731a629d32
Nduati Njoro <jiranimwema202420@gmail.com>
2025-12-10 10:27:00 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "clucktrack-6i22",
-  "appId": "1:95995963953:web:06b521b4f4f3b79417387d",
-  "apiKey": "AIzaSyC_jGg4gkjfldksalkdfjaoijwelkjsad",
-  "authDomain": "clucktrack-6i22.firebaseapp.com",
-  "measurementId": "G-5G6H7J8K9L",
-  "messagingSenderId": "95995963953",
-  "storageBucket": "clucktrack-6i22.appspot.com"
+  "apiKey": "AIzaSyCqmFf0AOyqkWylD0StUQEtlh3znclenVk",
+  "appId": "1:469670719794:web:fe6cb964108a3648f8e72b",
+  "authDomain": "studio-8423975791-c07ed.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "469670719794",
+  "projectId": "studio-8423975791-c07ed"
 };
